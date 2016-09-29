654e1c05d57b87c607f7a4989a0716683a3ee0be
JiyanBlack <rglsm7655558@gmail.com>
2016-09-29 03:15:04 +0000

    temp saved branch

--- demo.c
+++ demo.c
@@ -8,11 +8,13 @@ char *paddingWithZero(int num);
 
 int main()
 {
-    paddingWithZero(41);
+    int neighbour_length=611;
+    int cur_pair=(int)(neighbour_length*1000/4400.0);
+    printf("%d",cur_pair);
 }
 
 char *paddingWithZero(int num){
-    static char *str[4];
+    static char str[4];
     sprintf(str, "%d", num);
     int len=(int)strlen(str);
     printf("%d",len);
--- project1.c
+++ project1.c
@@ -12,18 +12,21 @@ int getKey(char line_numbers);
 double findMin(double array[4400]);
 int compFunc(const void *a, const void *b);
 void printCol(int col);
-int *transBlockString(char block);
-int * intdup(int const * src, size_t len);
+int *strToArray(char block);
+int *intdup(int const *src, size_t len);
+void generateBlocks(int col);
+void calcNeighbours(int col);
 
 //define core number below
-static const int core_number=8;
+static const int core_number = 8;
 
 static double data[500][4400];
 static long keys[4400];
 static const double dia = 0.000001;
-static int *neighbours[500];    //store the row number of all neighbours in a column
-static char *blocks[500];      //blocks[col][array of the rows, e.g:"1 6 7 9","2 4 8 19"]
-static long final_result[500]; //final result of the combination of row..
+static int *neighbours[500];      //store the row number of all neighbours in a column
+static int neighbour_length[500]; //store the length of all neighbours
+static char *blocks[500];         //blocks[col][array of the rows, e.g:"1 6 7 9","2 4 8 19"]
+static long final_result[500];    //final result of the combination of row..
 
 int main(void)
 {
@@ -33,47 +36,76 @@ int main(void)
     readData(data);
     readKeys(keys);
 
-    printf("Current dia is %f\n\n", dia);
-
+    printf("\nCurrent dia is %f.\n", dia);
+    printf("CPU core number is setted to %d.\n\n", core_number);
     omp_set_num_threads(core_number);
 #pragma omp parallel firstprivate(localSum)
     {
 #pragma omp for
-        for (int i = 0; i < 1; i++)
+        for (int col = 0; col < 1; col++)
         {
-            getBlocks(i);
+            getBlocks(col);
         }
     }
-    
 }
 
 void getBlocks(int col)
 {
+    calcNeighbours(col);
+    generateBlocks(col);
+}
+
+void generateBlocks(int col)
+{
+    for (int i = 0; i < neighbour_length[col]; i++)
+    {
+        printf("%d,%d\n", neighbours[col][2 * i], neighbours[col][2 * i + 1]);
+    }
+}
+
+int *quickSearch(int cur_pair, int isBigger, int result[], int col, int target_row, int result_index)
+{
+    int result[4400] = {-1};
+    //quickly search the target row neighbour
+    if (result_index == 0) //means it's the first time running
+    {
+        //find the rough index that the value would be
+        int cur_pair = (int)(neighbour_length[col] * 1000 / 4400.0);
+        if (target_row == neighbours[col][2 * cur_pair])
+        {
+            result[result_index]=neighbours[col][2 * cur_pair+1];
+        }
+    }
+}
+
+void calcNeighbours(int col)
+{
     //generate block lines as a string into the static blocks array
     int col_neighbours[212326];
-    int neighbour_index=0;
+    int neighbour_index = -1;
     for (int i = 0; i < 4400; i++)
     {
         for (int j = i + 1; j < 4400; j++)
         {
             if (isNeighbour(data[col][i], data[col][j]) == 1)
             {
-                col_neighbours[neighbour_index]=i;
-                col_neighbours[neighbour_index+1]=j;
-                printf("%d,%d",i,j);
-                neighbour_index+=2;
+                neighbour_index += 1;
+                col_neighbours[2 * neighbour_index] = i;
+                col_neighbours[2 * neighbour_index + 1] = j;
+                // printf("%d,%d\n", i, j);
             }
         }
     }
-    printf("col %d has neighbour pairs %d\n",col,neighbour_index+1);
-    neighbours[col]=col_neighbours;
+    neighbours[col] = col_neighbours;
+    neighbour_length[col] = neighbour_index + 1;
+    // printf("col %d has neighbour pairs %d\n", col, neighbour_length[col]);
 }
 
-int * intdup(int const * src, size_t len)
+int *intdup(int const *src, size_t len)
 {
-   int * p = malloc(len * sizeof(int));
-   memcpy(p, src, len * sizeof(int));
-   return p;
+    int *p = malloc(len * sizeof(int));
+    memcpy(p, src, len * sizeof(int));
+    return p;
 }
 
 int isNeighbour(double a, double b)
@@ -192,7 +224,7 @@ void printCol(int col)
     }
 }
 
-int *transBlockString(char block)
+int *strToArray(char block)
 {
     static int one_block[4];
     int index = 0;
